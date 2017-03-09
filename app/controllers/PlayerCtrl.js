ee5e354df2c6a655cef3f183062fe2b6fd0be19b
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-03-09 10:01:31 -0600

    still working

--- app/controllers/PlayerCtrl.js
+++ app/controllers/PlayerCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller("PlayerCtrl", function($scope, $routeParams) {
+app.controller("PlayerCtrl", function($scope, $routeParams, $mdSidenav) {
     console.log('$routeParams.chosen = ', $routeParams.chosen);
     $scope.chosen = $routeParams.chosen;
 });
